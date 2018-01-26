ec0dc0c4dae4878a3199b3ed66785955b2d731c8
Oscar Perez <oscarperezsanchez@hotmail.com>
2018-01-26 10:55:49 +0100

    setup start and heroku port

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const hbs = require('hbs');
 const sclib = require('./xtlib/sc.js');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 
 
 var app = express();
@@ -34,9 +36,9 @@ app.use( (req, res, next ) => {
 });
 
 //middleware
-app.use ((req, res, next) => {
-  res.render('maintenance.hbs');
-});
+// app.use ((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -72,6 +74,6 @@ app.get('/bad', (req, res)=> {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
